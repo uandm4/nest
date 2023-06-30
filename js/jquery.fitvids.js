5ac313d77fe7cd402b1cd9b3bc79167a64a7eb1e
uandm4 <uandm@naver.com>
2023-06-30 13:59:35 +0900

    fitvids 1.1 ver.

--- js/jquery.fitvids.js
+++ js/jquery.fitvids.js
@@ -1,13 +1,11 @@
-/*global jQuery */
-/*jshint multistr:true browser:true */
+/*jshint browser:true */
 /*!
-* FitVids 1.0
+* FitVids 1.1
 *
-* Copyright 2011, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
 * Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
 * Released under the WTFPL license - http://sam.zoy.org/wtfpl/
 *
-* Date: Thu Sept 01 18:00:00 2011 -0500
 */
 
 (function( $ ){
